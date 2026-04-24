0a3b36047c222ccaed08729cc5e657e47bb887c8
Carlos Quintanilha <cquinta@gmail.com>
2026-04-24 16:13:14 -0300

    incluindo testes unitários mssub

--- mssum/app/test_main.py
+++ mssum/app/test_main.py
@@ -8,15 +8,15 @@ def test_read_root():
     """Testa se o endpoint raiz está respondendo corretamente."""
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Microsserviço de Subtração"}
+    assert response.json() == {"message": "Microsserviço de Soma"}
 
 def test_operacao_subtracao_sucesso():
     """Testa a operação de subtração com valores válidos."""
-    response = client.post("/sub", json={"a": 10, "b": 4})
+    response = client.post("/sum", json={"a": 10, "b": 4})
     assert response.status_code == 200
-    assert response.json() == {"result": 6, "op": "sub"}
+    assert response.json() == {"result": 14, "op": "sum"}
 
 def test_operacao_subtracao_invalida():
-    """Testa a operação de subtração com tipos de dados inválidos (ex: string em vez de int)."""
-    response = client.post("/sub", json={"a": "dez", "b": 4})
+    """Testa a operação de soma com tipos de dados inválidos (ex: string em vez de int)."""
+    response = client.post("/sum", json={"a": "dez", "b": 4})
     assert response.status_code == 422
