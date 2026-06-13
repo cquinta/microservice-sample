3ee89f2a4a7e72323e280914d6ec641a145015af
Carlos Quintanilha <cquinta@gmail.com>
2026-06-13 16:58:43 -0300

    ajustes para health probe

--- msapi/app/main.py
+++ msapi/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from .config import Config
 from .routers import allops
+from .routers import health
 
 import os
 
 app = FastAPI(tittle="Cal  Microsservice")
 
 app.include_router(allops.router)
+app.include_router(health.router)
+
 
 
 @app.get("/")
--- mssum/app/main.py
+++ mssum/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import Depends, FastAPI
 
 from .routers import sum
+from .routers import health
+
 
 app = FastAPI(tittle="Sum Microsservice")
 
 app.include_router(sum.router)
+app.include_router(health.router)
 
 @app.get("/")
 async def root():
